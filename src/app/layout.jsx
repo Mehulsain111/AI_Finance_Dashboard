5a871981fbca8c85fdbfda953058868ad983f5ce
Mehul Kumar sain <mehulkumarsain@gmail.com>
2026-09-17 13:29:12 +0530

    fix: suppress hydration warning on body tag for browser extension attributes (Grammarly)

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
           rel="stylesheet"
         />
       </head>
-      <body className="app-body antialiased">
+      <body className="app-body antialiased" suppressHydrationWarning>
       
         <Script id="theme-init" strategy="beforeInteractive">
           {`
